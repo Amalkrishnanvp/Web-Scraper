26b41a249bedfd1c46fd63fdb94f12dbdfec11e7
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-21 12:35:28 +0530

    Getted user enterd url value

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const contentSelector = document.querySelector("#content-selector");
+  const scrapBtn = document.querySelector(".scrap-btn");
+  const urlHolder = document.querySelector(".url-holder");
 
- 
+  scrapBtn.addEventListener("click", () => {
+    if (urlHolder.value) {
+      const givenUrl = urlHolder.value;
+      console.log(givenUrl);
+    } else {
+      alert("You must enter a UR to scrap!");
+    }
+  });
 });
